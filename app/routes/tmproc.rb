25bc501161d3a5e740b46e58d8b1c09e8410bdc1
Cody A. Ray <sites@codyaray.com>
2012-05-04 01:32:25 -0500

    Change format to 'Reply C to change your vote', add web support for contextual changes, and provide url for viewing vote tallies without first voting

--- app/routes/tmproc.rb
+++ app/routes/tmproc.rb
@@ -1,20 +1,30 @@
 app do
 
+  get '/artifacts/:id/votes' do
+    artifact = Artifact.get(params[:id])
+    return "Sorry, we don't have an artifact '#{params[:id]}'" if artifact.nil?
+
+    @likes = artifact.votes.uncancelled.like.count
+    @dislikes = artifact.votes.uncancelled.dislike.count
+    haml :votes
+  end
+
   # not very RESTful, but alas... TextMarks API
-  # Text 'Art001 Dislike' or 'Bond007 Like'
+  # Text 'Art001 Dislike' or 'Bond007 Like' (explicit)
+  # Text 'C' to Change your last vote (contextual)
   get '/users/:phone_number/votes/:message' do
     user = User.first_or_create(:phone => params[:phone_number])
     user.update(:phone_id => params[:serial])
 
     message = params[:message].split('+')
-    if message.size == 2
+    if message.size == 2      # explicit messaging
       id, rating = message.first.downcase, message.last.downcase.to_sym
-    elsif message.size == 1
+    elsif message.size == 1   # contextual messaging
+      return "Reply C to change your last vote" if message.first.upcase != 'C'
       last_vote = user.votes.last
-      id = last_vote.artifact.id
-      rating = (message.first.upcase == 'Y') ? :like : :dislike
-      return if last_vote.value == rating
       last_vote.cancel.save
+      id = last_vote.artifact.id
+      rating = change_vote(last_vote.value)
     end
 
     artifact = Artifact.get(id)
@@ -22,29 +32,37 @@ app do
 
     user.votes.create(:value => rating, :artifact => artifact)
 
-    # Questions composed such that Yes = Like and No = Dislike
     likes = artifact.votes.uncancelled.like.count
     dislikes = artifact.votes.uncancelled.dislike.count
     if likes > dislikes
       if rating == :like
-        msg = "You've just joined the crowd. Are you sure you want to do that?"
+        msg = "You're following the crowd. If you want to stand out reply C to change your vote."
       elsif rating == :dislike
-        msg = "Odd, you're not following the others. Would you like to?"
+        msg = "You're not in the majority right now. Reply C to change to the winning team."
       end
     elsif likes < dislikes
       if rating == :like
-        msg = "Odd, you're not following the others. Stay in the minority?"
+        msg = "Odd, you're not following the others. Stay in the minority? Or reply C to change your vote."
       elsif rating == :dislike
-        msg = "You've just joined the crowd. Help out the little guy?"
+        msg = "You've just joined the crowd. Why not help out the little guy and reply C to change your vote?"
       end
     else
-      if rating == :like
-        msg = "You just leveled the playing field. Did you mean to do that?"
-      elsif rating == :dislike
-        msg = "Darn, you just leveled the playing field. Do you want to change your vote?"
+      msg = "You just leveled the playing field. Did you mean to do that? If not reply C to change your vote."
+    end
+
+    "#{id}\nLikes: #{likes}\nDislikes: #{dislikes}\n\n#{msg}"
+  end
+
+  private
+
+    def change_vote(vote)
+      if vote == :like
+        :dislike
+      elsif vote == :dislike
+        :like
+      else
+        :unknown
       end
     end
 
-    "#{id}\nLikes: #{artifact.votes.like.count}\nDislikes: #{artifact.votes.dislike.count}\n\n#{msg}"
-  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,34 @@
+function sendVote(vote) {
+  $.ajax({
+    url: "/users/0/votes/" + vote,
+    success: function(output_string) {
+      var votes = output_string.split("\n").map(function(str) {
+        return str;
+      });
+      $("#up").replaceWith("<span id=\"up\" class=\"thumb\">" + votes[1].split(" ").pop() + "</span>");
+      $("#dn").replaceWith("<span id=\"dn\" class=\"thumb\">" + votes[2].split(" ").pop() + "</span>");
+      $("#msg").html("<span id=\"#msg\">" + votes[4] + "</span>");
+    }
+  });
+}
+
 var vote = function(rating, artifactId) {
   return function() {
-    $.ajax({
-      url: "/users/0/votes/" + artifactId + "+" + rating,
-      success: function(output_string) {
-        var votes = output_string.split("\n").map(function(str) {
-          return str.split(" ").pop();
-        });
-        $("#up").replaceWith("<span id=\"#up\" class=\"thumb\">" + votes[0] + "</span>");
-        $("#dn").replaceWith("<span id=\"#dn\" class=\"thumb\">" + votes[1] + "</span>");
+    sendVote(artifactId + "+" + rating);
+
+    $("#msg").dialog({
+      resizable: false,
+      height: 190,
+      modal: true,
+      title: "Ya sure?",
+      buttons: {
+        "Change It": function() {
+          sendVote("C");
+          $( this ).dialog( "close" );
+        },
+        "Keep It": function() {
+          $( this ).dialog( "close" );
+        }
       }
     });
   };
