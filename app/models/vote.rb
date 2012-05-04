7b4d7a8a55eb26438ad15b4d46094f54861f417e
Cody A. Ray <sites@codyaray.com>
2012-05-04 01:16:14 -0500

    Support 'Are you sure?' type contextual messaging in SMS

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,9 +4,21 @@ class Vote
   property :id,           Serial
   property :value,        Enum[:like, :dislike], :required => true
   property :submitted_at, DateTime, :default => lambda { |p,s| DateTime.now }
+  property :cancelled,    Boolean, :default => false
+  property :cancelled_at, DateTime
   
   belongs_to :user
   belongs_to :artifact
+
+  def cancel
+    self.cancelled = true
+    self.cancelled_at = DateTime.now
+    self
+  end
+
+  def cancelled?
+    self.cancelled
+  end
 
   def self.like
     all(:value => :like)
@@ -14,5 +26,13 @@ class Vote
 
   def self.dislike
     all(:value => :dislike)
+  end
+
+  def self.cancelled
+    all(:cancelled => true)
+  end
+
+  def self.uncancelled
+    all(:cancelled => false)
   end
 end
--- app/routes/tmproc.rb
+++ app/routes/tmproc.rb
@@ -1,16 +1,50 @@
 app do
+
   # not very RESTful, but alas... TextMarks API
   # Text 'Art001 Dislike' or 'Bond007 Like'
   get '/users/:phone_number/votes/:message' do
-    message = params[:message].split('+')
-    id, rating = message.first.downcase, message.last.downcase.to_sym
-
     user = User.first_or_create(:phone => params[:phone_number])
     user.update(:phone_id => params[:serial])
+
+    message = params[:message].split('+')
+    if message.size == 2
+      id, rating = message.first.downcase, message.last.downcase.to_sym
+    elsif message.size == 1
+      last_vote = user.votes.last
+      id = last_vote.artifact.id
+      rating = (message.first.upcase == 'Y') ? :like : :dislike
+      return if last_vote.value == rating
+      last_vote.cancel.save
+    end
+
     artifact = Artifact.get(id)
     return "Sorry, we don't have an artifact '#{id}'" if artifact.nil?
+
     user.votes.create(:value => rating, :artifact => artifact)
 
-    "Likes: #{artifact.votes.like.count}\nDislikes: #{artifact.votes.dislike.count}"
+    # Questions composed such that Yes = Like and No = Dislike
+    likes = artifact.votes.uncancelled.like.count
+    dislikes = artifact.votes.uncancelled.dislike.count
+    if likes > dislikes
+      if rating == :like
+        msg = "You've just joined the crowd. Are you sure you want to do that?"
+      elsif rating == :dislike
+        msg = "Odd, you're not following the others. Would you like to?"
+      end
+    elsif likes < dislikes
+      if rating == :like
+        msg = "Odd, you're not following the others. Stay in the minority?"
+      elsif rating == :dislike
+        msg = "You've just joined the crowd. Help out the little guy?"
+      end
+    else
+      if rating == :like
+        msg = "You just leveled the playing field. Did you mean to do that?"
+      elsif rating == :dislike
+        msg = "Darn, you just leveled the playing field. Do you want to change your vote?"
+      end
+    end
+
+    "#{id}\nLikes: #{artifact.votes.like.count}\nDislikes: #{artifact.votes.dislike.count}\n\n#{msg}"
   end
 end
